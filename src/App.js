0ccca27ae31b4da86d260573ae2db872818305fe
sayyed-ahsan <sayyed.ahsan.dev@gmail.com>
2022-10-25 14:31:12 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Stack } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stack direction="horizontal" gap={2}>
+        <Button as="a" variant="primary">
+          Button as link
+        </Button>
+        <Button as="a" variant="success">
+          Button as link
+        </Button>
+      </Stack>
     </div>
   );
 }
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCOZVvsqZ6VI0RMD2T6Ev8_1u9fZ0Q3z0w",
+    authDomain: "assignment-10-e47e9.firebaseapp.com",
+    projectId: "assignment-10-e47e9",
+    storageBucket: "assignment-10-e47e9.appspot.com",
+    messagingSenderId: "99461245578",
+    appId: "1:99461245578:web:7a0c6b722c016b1c7ddb73"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
