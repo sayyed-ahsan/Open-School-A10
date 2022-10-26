237e66f66c6783d884b4fbb140ca843825eed99a
sayyed-ahsan <sayyed.ahsan.dev@gmail.com>
2022-10-26 22:53:03 +0600

    environment key

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCOZVvsqZ6VI0RMD2T6Ev8_1u9fZ0Q3z0w",
-    authDomain: "assignment-10-e47e9.firebaseapp.com",
-    projectId: "assignment-10-e47e9",
-    storageBucket: "assignment-10-e47e9.appspot.com",
-    messagingSenderId: "99461245578",
-    appId: "1:99461245578:web:7a0c6b722c016b1c7ddb73"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
